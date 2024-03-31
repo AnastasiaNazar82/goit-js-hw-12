7a0d0766bcc033005db691d20499d4b5b10f77fb
AnastasiaNazar82 <148636770+AnastasiaNazar82@users.noreply.github.com>
2024-03-31 02:52:22 +0200

    add

---
+++ src/js/render-functions.js
@@ -0,0 +1,51 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { refs, hideLoader } from '../main.js';
+
+export function renderImages(imagesArr) {
+  if (imagesArr.length === 0) {
+    hideLoader();
+    iziToast.error({
+      message: `❌ Sorry, there are no images matching your search query. Please, try again!`,
+      theme: 'dark',
+      progressBarColor: '#FFFFFF',
+      color: '#EF4040',
+      position: 'topRight',
+    });
+  }
+  // refs.galleryElement.innerHTML = '';
+  //   } else {
+  const imgGallery = imagesArr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<li class="gallery-item">
+    <a class="gallery-link" href="${largeImageURL}">
+    <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
+    </a>
+    <div class="content-image">
+    <p class="content-item">Likes<span class="info-item">${likes}</span></p>
+    <p class="content-item">Views<span class="info-item">${views}</span></p>
+    <p class="content-item">Comments<span class="info-item">${comments}</span></p>
+    <p class="content-item">Downloads<span class="info-item">${downloads}</span></p>
+    </div
+    </li>`
+    )
+    .join('');
+  // refs.galleryElement.innerHTML = imgGallery;
+  // refs.galleryElement.insertAdjacentHTML(
+  //   'beforeend',
+  //   renderImages(data.hits)
+  // );
+  return imgGallery;
+}
---
+++ src/main.js
@@ -0,0 +1,124 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { getImg } from './js/pixabay-api';
+import { renderImages } from './js/render-functions';
+
+export const refs = {
+  galleryElement: document.querySelector('.gallery'),
+  form: document.querySelector('.form'),
+  loader: document.querySelector('.conteiner-loader'),
+  btnLoadMore: document.querySelector('.loadMore'),
+};
+
+const inputValue = document.querySelector('input');
+let currentPage = 1;
+let maxPage = 0;
+const preSize = 15;
+
+// ===========================================
+function showLoader() {
+  refs.loader.classList.remove('is-hidden');
+}
+
+export function hideLoader() {
+  refs.loader.classList.add('is-hidden');
+}
+// ============
+
+function showLoadMore() {
+  refs.btnLoadMore.classList.remove('is-hidden');
+}
+
+function hideLoadMore() {
+  refs.btnLoadMore.classList.add('is-hidden');
+}
+
+//===========================================
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.btnLoadMore.addEventListener('click', onLoadMoreClick);
+
+async function onFormSubmit(e) {
+  e.preventDefault();
+  hideLoadMore();
+  currentPage = 1;
+  const searchImgs = inputValue.value.trim();
+  refs.galleryElement.innerHTML = '';
+
+  if (!searchImgs) {
+    hideLoader();
+    iziToast.info({
+      message: ` Please fill in the field for search`,
+      position: 'topRight',
+    });
+    refs.galleryElement.innerHTML = '';
+    return;
+  }
+
+  try {
+    showLoader();
+    const data = await getImg(inputValue, currentPage);
+    maxPage = Math.ceil(data.totalHits / preSize);
+    refs.galleryElement.insertAdjacentHTML(
+      'beforeend',
+      renderImages(data.hits)
+    );
+    lightbox.refresh();
+  } catch (error) {
+    console.log(error);
+  }
+  hideLoader();
+  checkBtnStatus();
+  e.target.reset();
+}
+
+// ==========================================
+async function onLoadMoreClick() {
+  currentPage += 1;
+  showLoader;
+  try {
+    const data = await getImg(inputValue, currentPage);
+
+    refs.list.insertAdjacentHTML('beforeend', renderImages(data.hits));
+    lightbox.refresh();
+  } catch (error) {
+    console.log(error);
+  }
+  scroll();
+  checkBtnStatus();
+  hideLoader();
+}
+
+// ==============================================
+
+function checkBtnStatus() {
+  if (currentPage >= maxPage) {
+    hideLoadMore();
+    iziToast.info({
+      color: 'yellow',
+      message: "We're sorry, but you've reached the end of search results.",
+      position: 'topRight',
+    });
+  } else {
+    showLoadMore();
+  }
+}
+function scroll() {
+  const height = refs.gallery.firstElementChild.getBoundingClientRect().height;
+  scrollBy({
+    top: height * 2,
+    behavior: 'smooth',
+  });
+}
+
+// ===============================================
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+lightbox.refresh();
